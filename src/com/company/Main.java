39d6ddf7b9335b1cca83ad293f08d36449e8911a
Georgii <loveOlechka@mail.ru>
2021-12-22 16:46:33 +0300

    Сделал)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int bananaWeight = 400;
+        System.out.println(bananaWeight);
+        int milkWeight = 210;
+        System.out.println(milkWeight);
+        int icecreameWeight = 200;
+        System.out.println(icecreameWeight);
+        int eggWeight = 280;
+        System.out.println(eggWeight);
+        int generalWeight = bananaWeight + milkWeight + icecreameWeight + eggWeight;
+        System.out.println("Общий вес спорт завтрака " + generalWeight + " грамм");
+        System.out.println("Общий вес спорт завтрака " + generalWeight / 1000 + "кг");
     }
 }

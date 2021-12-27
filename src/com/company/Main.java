df3c3ae238f440ac2318ef8f7000569a6013a8d2
Georgii <loveOlechka@mail.ru>
2021-12-27 11:14:24 +0300

    Исправил )

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int bananaWeight = 400;
-        System.out.println(bananaWeight);
-        int milkWeight = 210;
-        System.out.println(milkWeight);
-        int icecreameWeight = 200;
-        System.out.println(icecreameWeight);
-        int eggWeight = 280;
-        System.out.println(eggWeight);
-        float generalWeight = bananaWeight + milkWeight + icecreameWeight + eggWeight;
-        System.out.println("Общий вес спорт завтрака " + generalWeight + " грамм");
-        System.out.println("Общий вес спорт завтрака " + generalWeight / 1000 + "кг");
+        int bananaAmount = 400;
+        System.out.println(bananaAmount);
+        int milkAmount = 210;
+        System.out.println(milkAmount);
+        int icecreameAmount = 200;
+        System.out.println(icecreameAmount);
+        int eggAmount = 280;
+        System.out.println(eggAmount);
+        float generalAmount = bananaAmount + milkAmount + icecreameAmount + eggAmount;
+        System.out.println("Общий вес спорт завтрака " + generalAmount + " грамм");
+        System.out.println("Общий вес спорт завтрака " + generalAmount / 1000 + "кг");
     }
 }
